c67768943fe0c38948b459fe5c867b1c470c2e76
Pehrsonal <55282098+Pehrsonal@users.noreply.github.com>
2021-02-22 11:37:48 +0100

    Updated BAR/Testing it in main
    added settings for show % and show time. Also fixed size works now. Edited with some more functions as well so better structure

--- main.go
+++ main.go
@@ -3,54 +3,132 @@ package main
 import (
 	"fmt"
 	"strings"
+	"sync"
 	"time"
 )
 
-type theBar struct {
-	whatPercent   int
-	curPercent    int
-	rePrint       string
-	loading       string
-	loadingLenght int
-	max           int
+type TheBar struct {
+	barWidth       int
+	value          int
+	maxValue       int
+	startTime      time.Time
+	theme          Style
+	showPercentage bool
+	showTime       bool
+	isFinished     bool
+	lock           sync.Mutex
 }
 
-func (theBar *theBar) createBar(current, max int) {
-	theBar.curPercent = current
-	theBar.loading = "#"
-	theBar.loadingLenght = 40
-	theBar.max = max
-	theBar.whatPercent = theBar.getPercent()
+type Style struct {
+	StartChar    rune
+	EndChar      rune
+	ProgressChar rune
 }
 
-func (theBar *theBar) getPercent() int {
-	return int(float32(theBar.curPercent) / float32(theBar.max) * 100)
-}
+func New(maxValue int) *TheBar {
 
-func (theBar *theBar) update(current int) {
-	theBar.curPercent = current
-	last := theBar.whatPercent
-	theBar.whatPercent = theBar.getPercent()
-	if theBar.whatPercent != last {
-		theBar.rePrint = strings.Repeat(theBar.loading, int(theBar.curPercent*theBar.loadingLenght/100))
+	theme := Style{
+		StartChar:    '{',
+		EndChar:      '}',
+		ProgressChar: '#',
 	}
-	fmt.Printf("[%-50s]%3d%% %4d/%d\n", theBar.rePrint, theBar.whatPercent, theBar.curPercent, theBar.max)
+
+	bar := &TheBar{
+		barWidth:       50,
+		value:          0,
+		maxValue:       maxValue,
+		theme:          theme,
+		showPercentage: true,
+		showTime:       true,
+		isFinished:     false,
+	}
+
+	return bar
+}
+
+func (b *TheBar) update(i int) {
+
+	if b.isFinished {
+		return
+	}
+
+	level := b.barWidth * i / b.maxValue
+	progress := strings.Repeat(string(b.theme.ProgressChar), level)
+	blanks := strings.Repeat(" ", b.barWidth-level)
+
+	fmt.Printf("\rProgress: %s%s%s%s", string(b.theme.StartChar), progress, blanks, string(b.theme.EndChar))
+
+	if b.showPercentage {
+		percentage := 100 * float32(i) / float32(b.maxValue)
+		fmt.Printf(" %.2f%%", percentage)
+	}
+	if b.showTime {
+		elapsed := time.Since(b.startTime).Seconds()
+		fmt.Printf(" - %.2fs ", elapsed)
+	}
+
+	b.value = i
+}
+
+func (b *TheBar) end() {
+
+	if b.isFinished {
+		return
+	}
+
+	b.update(b.maxValue)
+	b.isFinished = true
+
+	elapsed := time.Since(b.startTime)
+	fmt.Printf("\nTime it took: %fs\n", elapsed.Seconds())
+}
+
+func (b *TheBar) Set(i int) {
+	b.lock.Lock()
+	defer b.lock.Unlock()
+
+	if b.startTime.IsZero() {
+		b.Start()
+	}
+
+	if i >= b.maxValue {
+		b.end()
+	} else if i < 0 {
+		b.update(0)
+	} else {
+		b.update(i)
+	}
+}
+
+func (b *TheBar) Add(i int) {
+	b.Set(b.value + i)
+}
+
+func (b *TheBar) Increment() {
+	b.Add(1)
+}
+
+func (b *TheBar) Start() {
+	b.startTime = time.Now()
+	fmt.Printf("\n")
+	b.Set(0)
+}
+
+func StartNew(maxValue int) *TheBar {
+	bar := New(maxValue)
+	bar.Start()
+	return bar
+}
+
+func (b *TheBar) Finish() {
+	b.Set(b.maxValue)
 }
 
 func main() {
-
-	var start int = 0
-	var goal int = 140
-	var progressBar theBar
-	progressBar.createBar(start, goal)
-
-	for {
-		time.Sleep(15 * time.Millisecond)
-
-		if progressBar.whatPercent == 100 {
-			break
-		}
-		start = start + 1
-		progressBar.update(start)
+	b := StartNew(50)
+	for i := 0; i < 50; i++ {
+		b.Increment()
+		time.Sleep(20 * time.Millisecond)
 	}
+	b.Finish()
 }
